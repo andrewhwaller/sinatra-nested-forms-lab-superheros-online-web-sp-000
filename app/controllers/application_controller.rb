89f9e3b6df6d28c15864a9e3a2138ad68e4d5fa6
Andrew Waller <andrewhwaller@gmail.com>
2019-04-24 01:46:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,22 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
 
+    post '/teams' do
+      @team_name = params[team][name]
+      @team_motto = params[team][motto]
+      @member1_name = params[team][members][][name]
+      @member2_name = params[team][members][][name]
+      @member3_name = params[team][members][][name]
+      @member1_power = params[team][members][][power]
+      @member2_power = params[team][members][][power]
+      @member3_power = params[team][members][][power]
+      @member1_bio = params[team][members][][bio]
+      @member2_bio = params[team][members][][bio]
+      @member3_bio = params[team][members][][bio]
+      erb :team
+    end
 end
